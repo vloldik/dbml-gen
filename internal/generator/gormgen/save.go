e66552508e1f1d01956fe26cd0f87891a7e795bf
Vloldik <99754299+vloldik@users.noreply.github.com>
2024-10-06 17:41:05 +0300

    fix filename with _test suffix

--- internal/generator/gormgen/save.go
+++ internal/generator/gormgen/save.go
@@ -1,6 +1,8 @@
 package gormgen
 
 import (
+	"strings"
+
 	"github.com/dave/jennifer/jen"
 	"github.com/vloldik/dbml-gen/internal/utils/fileutil"
 	"github.com/vloldik/dbml-gen/internal/utils/strutil"
@@ -12,6 +14,10 @@ func saveFile(basePath, packageName, filename string, file *jen.File) error {
 	location := strutil.ConcantatePaths(basePath, packageName)
 	if err := fileutil.EnsureFolderExists(location, createFileMode); err != nil {
 		return err
+	}
+
+	if strings.HasSuffix(filename, "_test") {
+		filename += "-model"
 	}
 
 	return file.Save(strutil.ConcantatePaths(location, filename+".go"))
